5b66e8868a1516dedd89f217da55bccdf7a604ad
Brent Boghosian <brent.boghosian@remote-learner.net>
2013-06-19 11:15:24 -0400

    ELIS 2.4.2.3 release

--- addons/blocks/curr_admin/version.php
+++ addons/blocks/curr_admin/version.php
@@ -27,4 +27,4 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2013032200;
-$plugin->release = '2.4.2.2 (Build: 20130522)';
+$plugin->release = '2.4.2.3 (Build: 20130604)';
--- core/elis/program/plugins/userset_classification/version.php
+++ core/elis/program/plugins/userset_classification/version.php
@@ -3,5 +3,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2013032200;
-$plugin->release = '2.4.2.2 (Build: 20130522)';
+$plugin->release = '2.4.2.3 (Build: 20130604)';
 
